d1434f288f971a0972a59e95ce6f1541adb64f17
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-04 15:58:15 -0700

    switch from socket.io to raw websockets

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
-const io = require("socket.io")(3000);
+var WebSocketServer = require("websocket").server;
+var http = require("http");
 
-io.on("connect", (socket) => {
-  // either with send()
-  socket.send("Hello!");
+var server = http.createServer(function (request, response) {
+  console.log((new Date()) + " Received request for " + request.url);
+  response.writeHead(404);
+  response.end();
+});
+server.listen(3000, function () {
+  console.log((new Date()) + " Server is listening on port 3000");
+});
 
-  // or with emit() and custom event names
-  socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
+wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false,
+});
 
-  // handle the event sent with socket.send()
-  socket.on("message", (data) => {
-    // console.log(data);
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on("request", function (request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log(
+      (new Date()) + " Connection from origin " + request.origin + " rejected.",
+    );
+    return;
+  }
+
+  var connection = request.accept("tilt5-protocol", request.origin);
+  console.log((new Date()) + " Connection accepted.");
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      process.stdout.write(".");
+      // console.log("Received Message: " + message.utf8Data);
+      // connection.sendUTF(message.utf8Data);
+    } else if (message.type === "binary") {
+      // console.log(
+      //   "Received Binary Message of " + message.binaryData.length + " bytes",
+      // );
+      // connection.sendBytes(message.binaryData);
+    }
   });
-
-  // handle the event sent with socket.emit()
-  socket.on("salutations", (elem1, elem2, elem3) => {
-    console.log(elem1, elem2, elem3);
+  connection.on("close", function (reasonCode, description) {
+    console.log(
+      (new Date()) + " Peer " + connection.remoteAddress + " disconnected.",
+    );
   });
 });
